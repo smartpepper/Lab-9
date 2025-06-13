51f50a598dc3ab12fb2e5d70a829bcc81ec46f1d
Andrey Pertcev <school299.frn@gmail.com>
2025-06-13 17:09:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
-app = Flask('Furniture store')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app = Flask('Company Management')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///company.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-# products = [
-#     {'prod_name': 'sofa',
-#      'price': 12000,
-#      'in_stock': False,
-#      'id': 0},
-#     {'prod_name': 'table',
-#      'price': 6000,
-#      'in_stock': True,
-#      'id': 1},
-#     {'prod_name': 'chair',
-#      'price': 8000,
-#      'in_stock': False,
-#      'id': 2},
-# ]
-
-
-class Product(db.Model):
+class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    prod_name = db.Column(db.String(300))
-    price = db.Column(db.Integer)
-    in_stock = db.Column(db.Boolean, default=True)
+    company_name = db.Column(db.String(300), nullable=False)
+    work_duration = db.Column(db.Integer) 
+    is_current = db.Column(db.Boolean, default=True)
 
     def __repr__(self):
-        return f'Product{self.id}. {self.prod_name} - {self.price} rub.'
-
+        return f'Company {self.id}. {self.company_name} - {self.work_duration} months'
 
 @app.route('/')
 def main():
-    products = Product.query.all()
-    return render_template('index.html', products_list=products)
+    companies = Company.query.all()
+    return render_template('index.html', companies_list=companies)
 
+@app.route('/clear', methods=['DELETE'])
+def clear_companies():
+    try:
+        db.session.query(Company).delete()
+        db.session.commit()
+        return 'OK', 200
+    except Exception as e:
+        db.session.rollback()
+        return str(e), 500
 
-@app.route('/in_stock/<product_id>', methods=['PATCH'])
-def modify_product(product_id):
-    product = Product.query.get(product_id)
-    product.in_stock = request.json['in_stock']
+@app.route('/current/<company_id>', methods=['PATCH'])
+def modify_company(company_id):
+    company = Company.query.get(company_id)
+    if not company:
+        return jsonify({'error': 'Company not found'}), 404
+    company.is_current = request.json['is_current']
     db.session.commit()
-    # global products
-    # in_stock = request.json['in_stock']
-    # for product in products:
-    #     if product['id'] == product_id:
-    #         product.update({'in_stock': in_stock})
-    # return 'OK'
-
+    return 'OK', 200
 
 @app.route('/add', methods=['POST'])
-def add_product():
+def add_company():
     data = request.json
-    product = Product(**data)
-    db.session.add(product)
+    company = Company(
+        company_name=data['company_name'],
+        work_duration=data['work_duration'],
+        is_current=data.get('is_current', True)
+    )
+    db.session.add(company)
     db.session.commit()
-
-    # id_last = products[-1]['id']
-    # id_new = id_last + 1
-    # data['id'] = id_new
-    # products.append(data)
-    return 'OK'
-
-
+    return jsonify({'id': company.id}), 201
 
 if __name__ == '__main__':
     with app.app_context():
